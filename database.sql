432d41207f0585ce4c0171ad4bdfe78f7868ca33
Tony Wu <tonywu9401@gmail.com>
2020-06-06 20:44:41 -0700

    added insert statements

--- database.sql
+++ database.sql
@@ -101,13 +101,11 @@ CREATE TABLE Fine
     Fine_ID CHAR(255) PRIMARY KEY,
     Officer_ID CHAR(255) NOT NULL,
     Customer_ID CHAR(255) NOT NULL,
-    Fine_Type CHAR(255) NOT NULL,
     Issue_Date_Time TIMESTAMP NOT NULL,
     Must_Be_Paid_By_Date TIMESTAMP NOT NULL,
     Date_Time_Paid_On TIMESTAMP,
     FOREIGN KEY (Officer_ID) REFERENCES Officer(Officer_ID),
-    FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID),
-    FOREIGN KEY (Fine_Type) REFERENCES Fine_Type_Cost(Fine_Type)
+    FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID)
 );
 
 CREATE TABLE Fine_ID_Fine_Type
@@ -162,3 +160,41 @@ CREATE TABLE Parking_Pass
     FOREIGN KEY (Space_ID, Lot_ID) REFERENCES Parking_Space(Space_ID,Lot_ID) ON DELETE CASCADE,
     FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID) ON DELETE CASCADE
 );
+
+
+
+INSERT INTO Officer VALUES ('E1', 'O1', 'Day','M1');
+INSERT INTO Officer VALUES ('E2', 'O2', 'Day', 'M1');
+INSERT INTO Officer VALUES ('E3', 'O3', 'Night', 'M4');
+INSERT INTO Officer VALUES ('E4', 'O4', 'Night', 'M4');
+INSERT INTO Officer VALUES ('E5', 'O5', 'Night', 'M4');
+
+INSERT INTO Fine_Type_Cost VALUES ('Over Time Limit', 60.00);
+INSERT INTO Fine_Type_Cost VALUES ('Parking In Non-designated Space', 150.00);
+INSERT INTO Fine_Type_Cost VALUES ('Parking In Handicap Space', 250.00);
+INSERT INTO Fine_Type_Cost VALUES ('Parking In Emergency Space', 500.00);
+INSERT INTO Fine_Type_Cost VALUES ('No Payment', 100.00);
+
+INSERT INTO Fine VALUES ('F1', 'O1', 'C1', '2020-05-29 07:00:00', '2020-06-05', '2020-06-03 18:15:00');
+INSERT INTO Fine VALUES ('F2', 'O1', 'C5', '2020-05-29 12:00:00', '2020-06-05', 'NULL');
+INSERT INTO Fine VALUES ('F3', 'O2', 'C7', '2020-06-01 15:03:00', '2020-06-08', 'NULL');
+INSERT INTO Fine VALUES ('F4', 'O3', 'C8', '2020-06-09 19:00:00', '2020-06-16', '2020-06-10 13:00:00');
+INSERT INTO Fine VALUES ('F5', 'O5', 'C10', '2020-06-09 20:13:00', '2020-06-16', 'NULL');
+
+INSERT INTO Fine_ID_Fine_Type VALUES  ('F1', 'Parking In Handicap Space');
+INSERT INTO Fine_ID_Fine_Type VALUES  ('F2', 'Over Time Limit ');
+INSERT INTO Fine_ID_Fine_Type VALUES  ('F3', 'Over Time Limit ');
+INSERT INTO Fine_ID_Fine_Type VALUES  ('F4', 'Over Time Limit ');
+INSERT INTO Fine_ID_Fine_Type VALUES  ('F5', 'Parking In Emergency Vehicle Space');
+
+INSERT INTO Costs VALUES ('L1', 11, 'R1');
+INSERT INTO Costs VALUES ('L2', 12, 'R1');
+INSERT INTO Costs VALUES ('L3', 14, 'R3');
+INSERT INTO Costs VALUES ('L4', 14, 'R4');
+INSERT INTO Costs VALUES ('L5', 15, 'R4');
+
+INSERT INTO Patrols VALUES ('O1', 'L1');
+INSERT INTO Patrols VALUES ('O2', 'L2');
+INSERT INTO Patrols VALUES ('O3', 'L2');
+INSERT INTO Patrols VALUES ('O4', 'L4');
+INSERT INTO Patrols VALUES ('O5', 'L5');

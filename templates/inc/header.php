23204e42aa7d3fc8ad85e78cf7bff476ba2c9c0f
Sassan Shokoohi <sassan_shokoohi@me.com>
2020-06-14 11:56:23 -0700

    change "Company name" to "Parking Lot System"

--- templates/inc/header.php
+++ templates/inc/header.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
+  <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Parking Lot System</a>
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>

534cce5d59bdc2e07aba0208229b1ba16449ed09
Tony Wu <tonywu9401@gmail.com>
2020-06-14 00:39:48 -0700

    select fields for lot id and rates

--- rate.php
+++ rate.php
@@ -5,7 +5,8 @@ $rate = new Rate;
 $template = new Template('templates/rateTemplate.php');
 $template -> title = 'Rates';
 
+$template -> lotID = isset($_GET['lotID']) ? $_GET['lotID'] : null;
 $template -> rateType = isset($_GET['rate']) ? $_GET['rate'] : null;
-$template -> rates = $rate->getRatesForLot('L3', $template->rateType);
+$template -> rates = $rate->getRatesForLot($template->lotID, $template->rateType);
 
 echo $template;
--- templates/rateTemplate.php
+++ templates/rateTemplate.php
@@ -1,15 +1,29 @@
 <?php include 'inc/header.php'; ?>
 
+<form method="GET" action="rate.php">
 <div class="form-group">
-    <form method="GET" action="rate.php">
+        <label>Choose Lot ID</label>
+        <select name="lotID" class="form-control">
+            <option value="0"> </option>
+            <option value="L3">L3</option>
+        </select>
+    </div>
+    <div class="form-group">
+        <label>Choose Rate Type</label>
         <select name="rate" class="form-control">
-            <option value="0">Choose Rate</option>
+            <option value="0"> </option>
             <option value="Hourly_Rate">Hourly Rate</option>
             <option value="Day_Rate">Day Rate</option>
             <option value="Monthly_Rate">Monthly Rate</option>
         </select>
-        <input type="submit" class="btn btn-primary" value="Find Rate">
-    </form>
+    </div>
+    <button type="submit" class="btn btn-primary">Find Rate</button>
+</form>
+<form>
+
+
+
+</form>
 </div>
 
 <div class="rate-table">

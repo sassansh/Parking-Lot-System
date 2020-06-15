80e0d5dc3e05f7895eb1a9f026b34e73cf4daa0f
Tony Wu <tonywu9401@gmail.com>
2020-06-14 23:11:21 -0700

    getter for all rates and parking lots

--- rate.php
+++ rate.php
@@ -2,6 +2,7 @@
 
 <?php 
 $rate = new Rate;
+$parkingLot = new ParkingLot;
 $template = new Template('templates/rateTemplate.php');
 $template -> title = 'Rates';
 
@@ -18,9 +19,9 @@ if(isset($_POST['update'])) {
 
 $template -> allRates = $rate->getAllRates();
 
-
+$template -> parkingLots = $parkingLot->getParkingLots();
 $template -> lotID = isset($_GET['lotID']) ? $_GET['lotID'] : null;
 $template -> rateType = isset($_GET['rate']) ? $_GET['rate'] : null;
-$template -> rates = $rate->getRatesForLot($template->lotID, $template->rateType);
+$template -> selectedRates = $rate->getRatesForLot($template->lotID, $template->rateType);
 
 echo $template;

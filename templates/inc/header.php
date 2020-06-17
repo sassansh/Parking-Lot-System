ffa3bafba20a17282e24f57a170e9ce6544bfeff
Tony Wu <tonywu9401@gmail.com>
2020-06-16 23:37:32 -0700

    active state js

--- templates/inc/header.php
+++ templates/inc/header.php
@@ -2,14 +2,6 @@
 <html lang="en">
 
 <head>
-<<<<<<< Updated upstream
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Parking Lot System</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/styles.css">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
-=======
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $title ?></title>
@@ -31,7 +23,6 @@
       });
     });
   </script>
->>>>>>> Stashed changes
 </head>
 
 <body>

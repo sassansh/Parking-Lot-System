dd6c0d4acd960b84ef7ab014c4910e478d922ae7
Tony Wu <tonywu9401@gmail.com>
2020-06-16 22:08:15 -0700

    fixed deletion error message

--- customer.php
+++ customer.php
@@ -27,9 +27,9 @@ if(isset($_POST['pass_del_id'])) {
 if(isset($_POST['cus_del_id'])) {
     $del_id = $_POST['cus_del_id'];
     if($customer->delete($del_id)) {
-        redirect('customers.php', 'Parking Pass Deleted', 'success');
+        redirect('customers.php', 'Customer Deleted', 'success');
     } else {
-        redirect('customer.php?id='.$del_id, 'Parking Pass Not Deleted', 'error');
+        redirect('customer.php?id='.$del_id, 'Customer Not Deleted', 'error');
     }
 }
 

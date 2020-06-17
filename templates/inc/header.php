25f62e8be34e1e552006cb6e3b239510ef5975d9
Sassan Shokoohi <sassan_shokoohi@me.com>
2020-06-16 19:34:21 -0700

    favicon added

--- templates/inc/header.php
+++ templates/inc/header.php
@@ -6,6 +6,7 @@
     <title>Parking Lot</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/styles.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
 </head>
 <body>
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">

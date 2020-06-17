39e96e92765050c1b1ffd7f28e63ed3183b8117b
Tony Wu <tonywu9401@gmail.com>
2020-06-16 22:37:34 -0700

    cleaned up the officer code
    note of the purpose of the giant if else statement. seems like it works without it

--- templates/officerTemplate.php
+++ templates/officerTemplate.php
@@ -4,55 +4,36 @@
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Officers</h1>
     </div>
-<?php
-if($show == 'allLotOfficers'){
-    echo '<form method="GET" action="officer.php">
-    <button name="show" type="submit" id="show" class="btn btn-secondary" value="allOfficers">Show All Officers</button>
-</form>
-<br>
-<form method="GET" action="officer.php">
-    <button name="show" type="submit" id="show" class="btn btn-primary" value="Show Officers Patrolling All Lots">Show Officers Patrolling All Lots</button>
-</form>
-<br>
-<h5>Currently showing officers that patrol all lots:</h5>
-<br>
-';
-} else {
-    echo '<form method="GET" action="officer.php">
-    <button name="show" type="submit" id="show" class="btn btn-primary" value="allOfficers">Show All Officers</button>
-</form>
-<br>
-<form method="GET" action="officer.php">
-<button name="show" type="submit" id="show" class="btn btn-secondary" value="allLotOfficers">Show Officers Patrolling All Lots</button>
-</form>
-<br>
-<h5>Currently showing all Officers:</h5>
-<br>
-';
-}
-?>
-
-
-
-<div class="officer-table table-responsive">
+    <div style="display:flex">
+        <form method="GET" action="officer.php">
+            <button name="show" type="submit" id="show" class="btn btn-primary" value="allOfficers" style="margin-right: 20px;">Show All Officers</button>
+        </form>
+        <form method="GET" action="officer.php">
+            <button name="show" type="submit" id="show" class="btn btn-secondary" value="allLotOfficers">Show Officers Patrolling All Lots</button>
+        </form>
+    </div>
+    <br>
+    <h5>Currently showing all Officers:</h5>
+    <br>
+    <div class="officer-table table-responsive">
         <table class="table table-striped table-sm">
             <thead>
-        <tr>
-            <th>Officer ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Shift</th>
-        </tr>
-</thead>
-        <?php foreach ($officers as $officer) : ?>
-            <tr>
-                <td><?php echo $officer->Officer_ID; ?></td>
-                <td><?php echo $officer->First_Name; ?></td>
-                <td><?php echo $officer->Last_Name; ?></td>
-                <td><?php echo $officer->Shift; ?></td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
-</div>
+                <tr>
+                    <th>Officer ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Shift</th>
+                </tr>
+            </thead>
+            <?php foreach ($officers as $officer) : ?>
+                <tr>
+                    <td><?php echo $officer->Officer_ID; ?></td>
+                    <td><?php echo $officer->First_Name; ?></td>
+                    <td><?php echo $officer->Last_Name; ?></td>
+                    <td><?php echo $officer->Shift; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </div>
 </main>
 <?php include 'inc/footer.php'; ?>
